3b369f417ec7d4931e9d545c5c710b448d5630f4
onestep87 <philip88088@gmail.com>
2023-01-26 17:49:17 +0100

    Configuring Mapper and adding it to our Actions

---
+++ Core MVC pet project/Configurations/MapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Core_MVC_pet_project.Data;
+using Core_MVC_pet_project.Models;
+
+namespace Core_MVC_pet_project.Configurations
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<LeaveType, LeaveTypeVM>().ReverseMap();
+        }
+    }
+}
